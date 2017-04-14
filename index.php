0bf5cf92f2ed86c46186fa4e08b4f8aa57d00dfd
Deepanshu Sharma <iamds96+git@gmail.com>
2017-04-15 03:25:53 +0530

    Alpha version 0.0.1, not working

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+
+
+$string = 'newy';
+$res = strtolower(preg_replace('/[^a-zA-Z0-9]/','', $string));
+
+    function alpha (){
+        global $res;
+      $len = rand(4,88);
+      echo $len;
+      $arr= array();
+      $c = 0;
+      floop:
+        for($i=0;$i<strlen($res);$i++){
+          $y =rand(0,1);
+              if(rand(0,1) ==1){
+                array_push($arr,$res[$i]);
+                $c++;
+                if($c==$len) {echo 'break' ; break;}
+              }
+        }
+        if($c<4){echo "__$c __</br>",implode($arr),"</br>";goto floop;}
+
+        $res = implode($arr);
+
+        echo "</br> path: /", $res;
+      }
+
+      function getid()
+        { global $conn;
+          $uid = rand(10000,99999);
+          $sql = "SELECT  uid FROM shorturl WHERE uid = $uid;";
+          $result = mysqli_query($conn, $sql);
+          if (mysqli_num_rows($result) > 0)
+          {$uid = getid(); }// recall to get unique if not
+
+        return $uid;
+        }
+
+
+      function shorten(){//get a short path
+          $uid = getid(); // generate random unique id
+          echo  "</br>" . $short =  base_convert($uid, 16, 32)."</br>" ;
+        }
+
+        shorten();
+
+    //  alpha();
+/*$sql = "INSERT INTO shorturl (short, orig)
+VALUES ('Cardinadl', 'Tom B. Erichsen');";
+if ($conn->query($sql) === TRUE) {
+    echo "</br>New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}*/
+
+$conn->close();
+
+
+
+
+?>
