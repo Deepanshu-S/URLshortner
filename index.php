57bd0e8b249b0439993094d84bc93cc6be5d80bf
Deepanshu Sharma <iamds96+git@gmail.com>
2017-04-15 19:44:13 +0530

    First working release, ver 0.1.0

--- index.php
+++ index.php
@@ -3,21 +3,49 @@ $servername = "127.0.0.1";
 $username = "root";
 $password = "";
 $dbname = "test";
-
 // Create connection
 $conn = new mysqli($servername, $username, $password,$dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+  }
+
+  //Assign values passed by forms
+  $q = $_GET['q'];
+  $short = $_GET['short_url'];
+
+function checkprev(){
+      global $q,$short, $uid, $conn;
+    $sql = "SELECT * FROM shorturl where short='$short' ";
+    $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+          echo "Short URL already taken, try something else?";
+        } else {        $sql = "SELECT * FROM shorturl where orig='$q' ";
+                      $result2 = $conn->query($sql);
+
+                      if ($result2->num_rows > 0) {while($row = $result2->fetch_assoc()) {
+                        echo "<p><br>Long url, already exists:<br><strong id=\"textresp1\">iamds.tk/". $row["short"] ."</strong><br></p>"  ; }
+                      }
+                      else { savetodb();}
+                }
+
 }
 
-$q = intval($_GET['q']);
+function savetodb()
+  {   global $q,$short, $uid, $conn;
+          $sql = "INSERT INTO `shorturl` (`uid`, `short`, `orig`) VALUES ('$uid', '$short', '$q');";
+        if ($conn->query($sql) === TRUE) {
+            echo "<p><br>Short path saved:<br> <strong id=\"textresp1\">iamds.tk/$short</strong><br></p>";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+  }
 
+/*$string = 'newy';
+$res = strtolower(preg_replace('/[^a-zA-Z0-9]/','', $string));*/
 
-$string = 'newy';
-$res = strtolower(preg_replace('/[^a-zA-Z0-9]/','', $string));
-
-    function alpha (){
+/*  function alpha (){
         global $res;
       $len = rand(4,88);
       echo $len;
@@ -37,27 +65,37 @@ $res = strtolower(preg_replace('/[^a-zA-Z0-9]/','', $string));
         $res = implode($arr);
 
         echo "</br> path: /", $res;
-      }
+      }*/
 
-      function getid()
-        { global $conn;
+function getid(){
+          global $conn;
           $uid = rand(10000,99999);
           $sql = "SELECT  uid FROM shorturl WHERE uid = $uid;";
           $result = mysqli_query($conn, $sql);
           if (mysqli_num_rows($result) > 0)
-          {$uid = getid(); }// recall to get unique if not
+          {$uid = getid(); }// recall to get unique id if not
 
-        return $uid;
+          return $uid;
+    }
+
+
+function shorten(){//get a short path
+          global $uid, $short;
+          $short =  base_convert($uid, 16, 32) ;
+
         }
 
 
-      function shorten(){//get a short path
+//Function calls start here
+
           $uid = getid(); // generate random unique id
-           $short =  base_convert($uid, 16, 32) ;
-           echo "<p>Short path: <strong>iamds.tk/$short</strong></p>";
+
+        if ($short==""){
+          shorten();
         }
 
-        shorten();
+        checkprev();    // checks previous records for shorturls and longurls
+
 
     //  alpha();
 /*$sql = "INSERT INTO shorturl (short, orig)
---
+++ redirect.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "test";
+// Create connection
+$con = mysql_connect($servername, $username, $password);
+if (!$con) {
+    die('Could not connect: ' . mysql_error());
+}
+mysql_select_db($dbname, $con);
+
+
+$id = $_GET['id'];
+
+
+// determine if short url is valid
+$query = "SELECT orig FROM shorturl WHERE short = '". $id ."'";
+
+$result = mysql_query($query) or die ("Error in query: $query " . mysql_error());
+$row = mysql_fetch_array($result);
+$url = $row['orig'];
+$num_results = mysql_num_rows($result);
+
+if ( $num_results > 0  ) {
+    header("HTTP/1.1 301 Moved Permanently");
+    header("Location: ". $url );
+	exit;
+} else {header("Location: ". "http://facebook.com/" );
+    // redirect to submit page or short URL not found page.
+}
+mysql_close($con);
+?>
